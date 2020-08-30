e3881b0d0ead313ff8538284e019d0e9f22b7a26
山本健輔 <yamamoto314@gmail.com>
2020-08-31 05:07:29 +0900

    routesにログアウト追加。 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root 'top#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
   root 'posts#index'
