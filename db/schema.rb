6c0e2707d8dba8ae0ab4fd63f32f4577898a31e9
hidemaru11 <do.first.jp@gmail.com>
2020-08-30 11:52:39 +0900

    Add not null to content on posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_022929) do
+ActiveRecord::Schema.define(version: 2020_08_30_024925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
+    t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
