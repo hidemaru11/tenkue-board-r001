0c186f91f2e4df6755e7c77a5a5de304eb3a4758
山本健輔 <yamamoto314@gmail.com>
2020-08-30 15:52:23 +0900

    ゲストログイン機能リファクタリング。 #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
+
+  def self.guest
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
