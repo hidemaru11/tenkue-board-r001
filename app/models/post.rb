4ecde797c486edeeeaa55ac8508fe2297a129e2f
hiroaki matsuoka <ro09pe05.kauru@gmail.com>
2020-08-31 14:52:29 +0900

    不要なメソッドの削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
-
-  def user_id
-    user.id
-  end
-
+  
 end
