6b16581448869b81d20ac01b5244a6a4f284d035
hidemaru11 <do.first.jp@gmail.com>
2020-09-01 12:55:00 +0900

    n+1問題解消のためincludes追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.includes(:user).order(created_at: :desc)
   end
 
   def new
