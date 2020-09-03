f863fad5511584cfe6619cecacc1f51f299b4cd9
有吉　貴紀 <>
2020-09-04 01:47:57 +0900

    2重定義しているコードの削除(Post.find)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,11 @@ class LikesController < ApplicationController
   before_action :set_post
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.likes.create(post_id: post.id)
+    current_user.likes.create(post_id: @post.id)
   end
 
   def destroy
-      post = Post.find(params[:post_id])
-      current_user.likes.find_by(post_id: post.id).delete
+      current_user.likes.find_by(post_id: @post.id).delete
   end
 
   private
