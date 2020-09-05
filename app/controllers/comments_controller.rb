0efc63f25bd22aa98ba65b0f1a65ac38a0140cd3
hidemaru11 <do.first.jp@gmail.com>
2020-09-06 02:49:49 +0900

    コメント編集機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,21 @@ class CommentsController < ApplicationController
    end
  end
 
+ def edit
+  @post = Post.find(params[:post_id])
+  @comment = Comment.find_by(post_id: @post.id, id: params[:id])
+ end
+
+ def update
+  @post = Post.find(params[:post_id])
+  @comment = Comment.find(params[:id])
+  if @comment.update(comment_params)
+    redirect_to @post
+  else
+    render 'layouts/comment_form'
+  end
+ end
+
  private
  def comment_params
    params.require(:comment).permit(:comment, :post_id, :user_id)
