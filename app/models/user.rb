d0ea31e982052057802630eca53423b5b184edf4
山本健輔 <yamamoto314@gmail.com>
2020-09-05 10:40:34 +0900

    userモデル、正規表現適用。 #11

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, presence: true,
+            format: { with: VALID_PASSWORD_REGEX,
+            message: "は半角英字と半角数字のいずれとも含まれ、8文字以上32文字以下である必要があります" }
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX }
   def self.guest
     user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザ') do |user|
       user.password = SecureRandom.urlsafe_base64
