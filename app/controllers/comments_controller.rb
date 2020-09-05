05bda0c498423f1cbe84eba4dd9decccbd35e964
hidemaru11 <do.first.jp@gmail.com>
2020-09-06 03:05:35 +0900

    コメント削除機能を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,44 @@
 class CommentsController < ApplicationController
 
- def new
-   @comment = Comment.new
- end
-
- def create
-   @comment = Comment.new(comment_params)
-   @post = Post.find_by(id: params[:comment][:post_id])
-   if @comment.save
-     redirect_to post_path(@post)
-   else
-     flash[:alert] = "コメントを(140文字以内で)入力してください。"
-     render "posts/show"
-   end
- end
-
- def edit
-  @post = Post.find(params[:post_id])
-  @comment = Comment.find_by(post_id: @post.id, id: params[:id])
- end
-
- def update
-  @post = Post.find(params[:post_id])
-  @comment = Comment.find(params[:id])
-  if @comment.update(comment_params)
-    redirect_to @post
-  else
-    render 'layouts/comment_form'
+  def new
+    @comment = Comment.new
   end
- end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find_by(id: params[:comment][:post_id])
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "コメントを(140文字以内で)入力してください。"
+      render "posts/show"
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find_by(post_id: @post.id, id: params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      redirect_to @post
+    else
+      render 'layouts/comment_form'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @post
+  end
 
  private
- def comment_params
-   params.require(:comment).permit(:comment, :post_id, :user_id)
- end
+  def comment_params
+    params.require(:comment).permit(:comment, :post_id, :user_id)
+  end
 end
